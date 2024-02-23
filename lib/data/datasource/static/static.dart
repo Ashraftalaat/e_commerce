67320df4606380308d20f72bdab5abc067be207a
Your NameAshraftalaat <atslook@gmail.com>
2024-02-23 12:28:16 +0200

    5

---
+++ lib/data/datasource/static/static.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/core/constant/imageasset.dart';
+
+import '../../model/onboardingmodel/onboardingmodel.dart';
+
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(
+      title: "Choose Product",
+      image: AppImageAsset.onBoardingImageOne,
+      body:
+          "We Have a 100k+Products Choose \n Your product from our E- \n commerce shop."),
+  OnBoardingModel(
+      title: "Easy & Safe Payment",
+      image: AppImageAsset.onBoardingImageTwo,
+      body:
+          "We Have a 100k+Products Choose \n Your product from our E- \n commerce shop."),
+  OnBoardingModel(
+      title: "Track Your Order",
+      image: AppImageAsset.onBoardingImageThree,
+      body:
+          "We Have a 100k+Products Choose \n Your product from our E- \n commerce shop."),
+  OnBoardingModel(
+      title: "Fast Delivery",
+      image: AppImageAsset.onBoardingImageFour,
+      body:
+          "We Have a 100k+Products Choose \n Your product from our E- \n commerce shop."),
+];
--- lib/view/screen/onboarding.dart
+++ lib/view/screen/onboarding.dart
@@ -1,14 +1,49 @@
+import 'package:e_commerce/core/constant/color.dart';
+import 'package:e_commerce/data/datasource/static/static.dart';
 import 'package:flutter/material.dart';
-
 
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(body: Container(
-      padding: const EdgeInsets.all(20),
-      child: const Text("hi"),),);
+    return Scaffold(
+      body: SafeArea(
+        child: PageView.builder(
+          itemCount: onBoardingList.length,
+          itemBuilder: (context, i) {
+            return Column(
+              children: [
+                const SizedBox(height: 50),
+                Text(
+                  onBoardingList[i].title!,
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                const SizedBox(height: 90),
+                Image.asset(
+                  onBoardingList[i].image!,
+                  fit: BoxFit.fill,
+                  height: 280,
+                ),
+                const SizedBox(height: 70),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    onBoardingList[i].body!,
+                    style: const TextStyle(
+                      color: AppColor.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      height: 2,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
-//===================
