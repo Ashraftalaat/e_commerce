c1899af8092f0fff5a0d7a7a0c2b0b0faf2939b3
Your NameAshraftalaat <atslook@gmail.com>
2024-02-25 17:20:07 +0200

    8

--- lib/routs.dart
+++ lib/routs.dart
@@ -2,7 +2,6 @@ import 'package:e_commerce/core/constant/routs.dart';
 import 'package:e_commerce/view/screen/login.dart';
 import 'package:flutter/material.dart';
 
-
 Map<String, Widget Function(BuildContext)> routes = {
-  AppNamesRouts.login :(context) =>  const Login()
+  AppNamesRouts.login: (context) => const Login()
 };
