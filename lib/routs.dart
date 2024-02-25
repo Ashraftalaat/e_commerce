ab1273194e991b73472f253c4c10f3bfb53846cd
Your NameAshraftalaat <atslook@gmail.com>
2024-02-25 17:54:12 +0200

    8

--- lib/routs.dart
+++ lib/routs.dart
@@ -2,7 +2,8 @@ import 'package:e_commerce/core/constant/routs.dart';
 import 'package:e_commerce/view/screen/login.dart';
 import 'package:flutter/material.dart';
 
+
+// يفضل الروت خارج constant لسهولة التنقل بين الصفحات
 Map<String, Widget Function(BuildContext)> routes = {
   AppNamesRouts.login: (context) => const Login()
 };
-//=======
